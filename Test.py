5a736012743b5ae096f3180ef9ad7337a3ed1df5
simrath-kaur <simrath.kaur@iiitb.ac.in>
2024-02-15 16:15:57 +0530

    pipeline

---
+++ Test.py
@@ -0,0 +1,9 @@
+import unittest
+from Prg1 import summation
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        data=[23,32]
+        result=summation(data)
+        self.assertEqual(result,55)
+if __name__== '__main__':
+    unittest.main()
